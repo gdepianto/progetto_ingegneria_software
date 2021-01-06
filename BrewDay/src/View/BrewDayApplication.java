c6402c97cc418301cab7f5911cf7ec9895ddb712
Anselmo Folcio <a.folcio@campus.unimib.it>
2021-01-06 16:55:35 +0100

    Aggiunta tabella ricette all'avvio dell'applicazione

--- BrewDay/src/View/BrewDayApplication.java
+++ BrewDay/src/View/BrewDayApplication.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import controller.ControllerIngredienti;
+import controller.ControllerRicetta;
 import model.Ingrediente;
 
 public class BrewDayApplication {
@@ -33,14 +34,35 @@ public class BrewDayApplication {
 	         System.exit(0);
 	      }
 	      System.out.println("Table created successfully");
-	
-	      ControllerIngredienti control1 = new ControllerIngredienti();
-	      Visualizzazione_Ingrediente finestra = new Visualizzazione_Ingrediente(control1);
-	      finestra.open();
-	      Aggiunta_Ingrediente finestraAgg = new Aggiunta_Ingrediente(control1);
-	      finestraAgg.open();
-	      }
-	}
+	      
+	      try {
+		        Class.forName("org.sqlite.JDBC");
+		        c = DriverManager.getConnection("jdbc:sqlite:brewday.db");
+		        System.out.println("Opened database successfully");
+		
+		        stmt = c.createStatement();
+		        String sql = "CREATE TABLE IF NOT EXISTS ricetta" + 
+		       		  "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+		       		  "nome VARCHAR(45) NOT NULL, " +
+		       		  "descrizione TEXT(1000) NOT NULL, " +
+		       		  "tempo_preparazione INT NOT NULL)";
+		        stmt.executeUpdate(sql);
+		        stmt.close();
+		        c.close();
+		     } catch ( Exception e ) {
+		        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		        System.exit(0);
+		     }
+		     System.out.println("Table created successfully");
+		     
+		      ControllerIngredienti control1 = new ControllerIngredienti();
+		      Visualizzazione_Ingrediente finestra = new Visualizzazione_Ingrediente(control1);
+		      finestra.open();
+		      Aggiunta_Ingrediente finestraAgg = new Aggiunta_Ingrediente(control1);
+		      finestraAgg.open();
+	  }	
+}
+		
 	
 	
 
