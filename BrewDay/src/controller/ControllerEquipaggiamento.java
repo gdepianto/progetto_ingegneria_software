4646725f47f44428fca2e9109a73932aa201f83e
Anselmo Folcio <a.folcio@campus.unimib.it>
2021-01-12 10:24:05 +0100

    Aggiornato Equipaggiamento, costruttore, controller e mapper

--- BrewDay/src/controller/ControllerEquipaggiamento.java
+++ BrewDay/src/controller/ControllerEquipaggiamento.java
@@ -1,5 +1,20 @@
 package controller;
 
+import database_layer.MapperEquipaggiamento;
+import model.Equipaggiamento;
+
 public class ControllerEquipaggiamento {
+	
+	private MapperEquipaggiamento mapperEquipaggiamento;
+	
+	public String aggiungiEquipaggiamento(String nome, float disponibilita) {
+		Equipaggiamento equip = new Equipaggiamento(nome, disponibilita);
+		return mapperEquipaggiamento.insert(equip);
+	}
+	
+	public void aggiornaEquipaggiamento(String nome, float disponibilita) {
+		Equipaggiamento equip = new Equipaggiamento(nome, disponibilita);
+		mapperEquipaggiamento.update(equip);
+	}
 
 }
