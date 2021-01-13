13272f918f2da39067ba544bae78f1621c96bd2a
Anselmo Folcio <a.folcio@campus.unimib.it>
2021-01-13 10:28:49 +0100

    Aggiunta tabella Quantita all'avvio della applicazioe

--- BrewDay/src/View/BrewDayApplication.java
+++ BrewDay/src/View/BrewDayApplication.java
@@ -82,60 +82,58 @@ public class BrewDayApplication {
 
 		     
 		     try {
-		        Class.forName("org.sqlite.JDBC");
-		        c = DriverManager.getConnection("jdbc:sqlite:brewday.db","",
-		                 BrewDayApplication.password);
-		        System.out.println("Opened database successfully");
-		
-		        stmt = c.createStatement();
-		        String sql = "CREATE TABLE IF NOT EXISTS equipaggiamento" + 
-		       		  "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-		       		  "nome VARCHAR(45) NOT NULL, " +
-		       		  "capacita FLOAT NOT NULL)";
-		        stmt.executeUpdate(sql);
-		        stmt.close();
-		        c.close();
-		     } catch ( Exception e ) {
-		        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		        System.exit(0);
-		     }
-		     System.out.println("Table created successfully");
-		     
-//		     try {
-//		        Class.forName("org.sqlite.JDBC");
-//		        c = DriverManager.getConnection("jdbc:sqlite:brewday.db","",
-//		                 BrewDayApplication.password);
-//		        System.out.println("Opened database successfully");
-//		
-//		        stmt = c.createStatement();
-//		        String sql = "CREATE TABLE IF NOT EXISTS quantita" + 
-//		       		  "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-//		       		  "ricetta NOT NULL, " +
-//		       		  "ingrediente NOT NULL)" +
-//		       		  "INDEX fk_ingrediente_has_ricetta_ricetta1_idx (ricetta_id ASC) VISIBLE," +
-//		       	      "INDEX fk_ingrediente_has_ricetta_ingrediente_idx (ingrediente_id ASC) VISIBLE,"+
-//		       	      "CONSTRAINT fk_ingrediente_has_ricetta_ingrediente"+
-//		       	      "FOREIGN KEY (ingrediente)"+
-//		       	      "REFERENCES brewDay.ingrediente (id)"+
-//		       	      "ON DELETE NO ACTION"+
-//		       	      "ON UPDATE NO ACTION"+
-//		       	      "CONSTRAINT fk_ingrediente_has_ricetta_ricetta1"+
-//		              "FOREIGN KEY ricetta)"+
-//		              "REFERENCES brewDay.ricetta (id)"+
-//		              "ON DELETE NO ACTION"+
-//		              "ON UPDATE NO ACTION)";
-//		        stmt.executeUpdate(sql);
-//		        stmt.close();
-//		        c.close();
-//		     } catch ( Exception e ) {
-//		        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-//		        System.exit(0);
-//		     }
-			System.out.println("Table created successfully");
-			ControllerEquipaggiamento controllerEq = new ControllerEquipaggiamento();
-			CreaEquipaggiamento finestraCreaEq = new CreaEquipaggiamento(controllerEq,pass);
-			finestraCreaEq.open();
+
+			        Class.forName("org.sqlite.JDBC");
+			        c = DriverManager.getConnection("jdbc:sqlite:brewday.db","",
+			                 BrewDayApplication.password);
+			        System.out.println("Opened database successfully");
+			
+			        stmt = c.createStatement();
+			        String sql = "CREATE TABLE IF NOT EXISTS equipaggiamento" + 
+			       		  "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+			       		  "nome VARCHAR(45) NOT NULL, " +
+			       		  "capacita FLOAT NOT NULL)";
+			        stmt.executeUpdate(sql);
+			        stmt.close();
+			        c.close();
+			     } catch ( Exception e ) {
+			        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			        System.exit(0);
+			     }
+			     System.out.println("Table created successfully");
 			     
+			     try {
+				        Class.forName("org.sqlite.JDBC");
+				        c = DriverManager.getConnection("jdbc:sqlite:brewday.db","",
+				                 BrewDayApplication.password);
+				        System.out.println("Opened database successfully");
+				
+				        stmt = c.createStatement();
+				        String sql = "CREATE TABLE IF NOT EXISTS quantita" + 
+				       		  "(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+				       		  "id_ricetta INTEGER NOT NULL, " +
+				       		  "id_ingrediente INTEGER NOT NULL," +
+				       	      "FOREIGN KEY (id_ingrediente) "+
+				       	      "REFERENCES ingrediente(id) "+
+				       	      "ON DELETE NO ACTION "+
+				       	      "ON UPDATE NO ACTION,"+
+				              "FOREIGN KEY (id_ricetta) "+
+				              "REFERENCES ricetta(id) "+
+				              "ON DELETE NO ACTION "+
+				              "ON UPDATE NO ACTION)";
+				        stmt.executeUpdate(sql);
+				        stmt.close();
+				        c.close();
+				     } catch ( Exception e ) {
+				        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+				        System.exit(0);
+				     }
+				     System.out.println("Table created successfully");
+
+			     
+		ControllerEquipaggiamento controllerEq = new ControllerEquipaggiamento();
+		CreaEquipaggiamento finestraCreaEq = new CreaEquipaggiamento(controllerEq,pass);
+		finestraCreaEq.open();
 			    
 	}
 	
